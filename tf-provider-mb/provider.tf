66dca99748cf27ea8ecf58e44168d67cef4dd1f8
guislander <lynn.d.calvo@gmail.com>
2020-07-14 18:16:19 -0500

    removed tf-provider folder

--- tf-provider-mb/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "matchbox" {
-  version = "0.3.0"
-  endpoint    = "matchbox.sabana.com:8081"
-  client_cert = file("~/.config/matchbox/client.crt")
-  client_key  = file("~/.config/matchbox/client.key")
-  ca          = file("~/.config/matchbox/ca.crt")
-}
