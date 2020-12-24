f4c7ad8e0649b6ae83d2c37ed9f771140f2bb90b
guislander <lynn.d.calvo@gmail.com>
2020-12-24 14:02:38 -0600

    mod index key

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "myk8s" {
 
   # configuration
   k8s_domain_name    = "node1.sabana.com"
-  ssh_authorized_key = file("~/.ssh/id_rsa.pub")[0]
+  ssh_authorized_key = (file("~/.ssh/id_rsa.pub"))[0]
 
   # machines
 
