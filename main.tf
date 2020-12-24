d3ed9099b6630576fcd12494d80c086aaf6912d0
guislander <lynn.d.calvo@gmail.com>
2020-12-24 14:08:27 -0600

    mod key file

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "myk8s" {
 
   # configuration
   k8s_domain_name    = "node1.sabana.com"
-  ssh_authorized_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCxyRa+tN0uDM5o4QZbWEQxZ1g09wwGuOWS9mfZzf0Gw9mCcsdl1rhyVHfSyv8qsTpZdmHaSr13TKtMxgEzK+Ay4PIae4emLMAz+YJRc0b+kRZkiq9yykdnPdnGp5ekTX8zTWwnWf0xHN9yna8MNscuKrs045jVBQ8tQ6KrF0OaseR0BnxBRJ6TBvK527cXtm5ANk7tq7hAq6/27OA4w1avaZh7AWa7pwdTekb5gE5+6UtpkGg1VDOsLSdhyonfJp4bHETD4/4bTHrxfhue4i7jvfDBjjunMVA/P6q5AZ0LQVBHv/t7S1trCc16KPZBf7qsPS4MQ+iJD1197iJtxdgG1lKz/gr7HB/mwtrylZv6F/6XnY87EZLyT5ulaSqPEAVv6xCiXHY5r3fxFYdnOM18LTBujZz8ZKYGQPkyXGpiL2DX7PWaRW3jvztq+/z6W9fSQqFH6XbIULs35JMsjZyV4jJKG4vi7yJFiz73BNB7bmIyYV4JpXaAT9hOgLHFHr0= lcalvo@GuIslander-MBP.sabana.com"
+  ssh_authorized_key = file("~/.ssh/id_rsa.pub")
 
   # machines
 
